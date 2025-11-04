3a8729e2a25e4ac2ed0395093640a1e6e2862151
Kwadwot <kwadwoptwumasi@gmail.com>
2025-11-03 23:56:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+});
+
